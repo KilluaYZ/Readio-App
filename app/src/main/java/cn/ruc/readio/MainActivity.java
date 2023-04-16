91cc1bd0e704f4d369c0f46ac4bd38be03c07291
Flandrexin <2363317668@qq.com>
2023-04-16 15:06:01 +0800

    init project

--- app/src/main/java/cn/ruc/readio/MainActivity.java
+++ app/src/main/java/cn/ruc/readio/MainActivity.java
@@ -1,19 +1,15 @@
 package cn.ruc.readio;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import cn.ruc.readio.databinding.ActivityMainBinding;
 
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_userpage,R.id.navigation_works,R.id.navigation_shelf,R.id.navigation_commend).build();
+                R.id.navigation_userpage, R.id.navigation_works, R.id.navigation_shelf, R.id.navigation_commend).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);

925bdbbc49068a0d077747639b30d9805b99ffeb
ziyang <2959243019@qq.com>
2023-04-19 01:10:21 +0800

    个人页面连接后端

--- app/src/main/java/cn/ruc/readio/ui/userpage/userPageFragment.java
+++ app/src/main/java/cn/ruc/readio/ui/userpage/userPageFragment.java
@@ -1,25 +1,90 @@
 package cn.ruc.readio.ui.userpage;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
 import cn.ruc.readio.databinding.FragmentUserpageBinding;
+import cn.ruc.readio.util.HttpUtil;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class userPageFragment extends Fragment {
 
     private FragmentUserpageBinding binding;
+    private String token = "";
+
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 1:
+                    Log.d(this.toString(),"修改userID");
+                    binding.userID.setText((String)msg.obj);
+                    break;
+                case 2:
+                    Log.d(this.toString(),"修改userName");
+                    binding.userName.setText((String)msg.obj);
+                    break;
+                default:
+            }
+
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentUserpageBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    if(token.isEmpty()){
+//                        login();
+//                    }
+//                    getProfile();
+//                } catch (JSONException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }).start();
+
+        try {
+            if(token.isEmpty()){
+                login();
+            }else{
+                Log.d(this.toString(),"token = "+token);
+                getProfile();
+            }
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
         return root;
     }
 
@@ -27,5 +92,66 @@ public class userPageFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void login() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("phoneNumber","18314266702");
+        jsonObject.put("passWord","123456");
+
+        HttpUtil.postRequestJson("/auth/app/login", jsonObject.toString(), new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Looper.prepare();
+                Toast.makeText(getContext(),"登录失败！请检查网络连接",Toast.LENGTH_LONG).show();
+                Looper.loop();
+                Log.e(this.toString(), "登录数据获取失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONObject responseJsonObject = new JSONObject(response.body().string());
+                    token = responseJsonObject.getString("token");
+                    Log.d(this.toString(), "获取到登录数据");
+                    Log.d(this.toString(),"token = "+token);
+                    getProfile();
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private void getProfile(){
+        HttpUtil.getRequestWithToken("/auth/app/profile", token, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Looper.prepare();
+                Toast.makeText(getContext(),"获取信息失败！请检查网络连接",Toast.LENGTH_LONG).show();
+                Looper.loop();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    JSONObject responseJsonObject = new JSONObject(response.body().string()).getJSONObject("data").getJSONObject("data");
+                    String userId = String.valueOf(responseJsonObject.getInt("userId"));
+                    String userName = responseJsonObject.getString("userName");
+//                    handler.obtainMessage(1,userId);
+//                    handler.obtainMessage(2,userName);
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            binding.userID.setText("ID:"+userId);
+                            binding.userName.setText(userName);
+                        }
+                    });
+                    Log.d(this.toString(),"拿到profile数据");
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/cn/ruc/readio/util/HttpUtil.java
+++ app/src/main/java/cn/ruc/readio/util/HttpUtil.java
@@ -1,5 +1,7 @@
 package cn.ruc.readio.util;
 
+import android.util.Log;
+
 import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -7,9 +9,12 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 
 public class HttpUtil {
-    private static String BASE_URL = "http://127.0.0.1:5000";
+    private static String BASE_URL = "http://[2401:ec00:13:b9c:191e:1dd9:565f:60b3]:5000";
 
+    //get方法向服务器请求，address是api地址 /auth/app/login
+    //callback是个回调函数
     public static void getRequest(String address, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address);
         Request request = new Request.Builder()
                 .url(BASE_URL + address)
                 .build();
@@ -17,7 +22,9 @@ public class HttpUtil {
         okHttpClient.newCall(request).enqueue(callback);
     }
 
+    //带令牌的get方法
     public static void getRequestWithToken(String address, String token, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address);
         Request request = new Request.Builder()
                 .url(BASE_URL + address)
                 .addHeader("Authorization", token)
@@ -26,7 +33,9 @@ public class HttpUtil {
         okHttpClient.newCall(request).enqueue(callback);
     }
 
+    //普通的post方法
     public static void postRequest(String address, RequestBody requestBody, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address);
         Request request = new Request.Builder()
                 .url(BASE_URL + address)
                 .post(requestBody)
@@ -35,7 +44,9 @@ public class HttpUtil {
         okHttpClient.newCall(request).enqueue(callback);
     }
 
+    //带令牌的post方法
     public static void postRequestWithToken(String address, String token, RequestBody requestBody, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address);
         Request request = new Request.Builder()
                 .url(BASE_URL + address)
                 .post(requestBody)
@@ -45,7 +56,9 @@ public class HttpUtil {
         okHttpClient.newCall(request).enqueue(callback);
     }
 
+    //普通的post方法，向服务器传json字符串
     public static void postRequestJson(String address, String jsonString, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address + "\n body = "+jsonString);
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonString);
 
         Request request = new Request.Builder()
@@ -56,7 +69,9 @@ public class HttpUtil {
         okHttpClient.newCall(request).enqueue(callback);
     }
 
+    //带令牌的post方法，向服务器传json字符串
     public static void postRequestWithTokenJson(String address, String token, String jsonString, Callback callback){
+        Log.d("HttpUtil","正在访问 "+BASE_URL + address);
         RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonString);
 
         Request request = new Request.Builder()

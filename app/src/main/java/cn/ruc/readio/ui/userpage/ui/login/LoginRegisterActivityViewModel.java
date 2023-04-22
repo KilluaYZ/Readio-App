c5efe33beda4e93073c4a9acbb9fc31364e16bf3
ziyang <2959243019@qq.com>
2023-04-22 14:50:21 +0800

    添加登录页

---
+++ app/src/main/java/cn/ruc/readio/ui/userpage/ui/login/LoginRegisterActivityViewModel.java
@@ -0,0 +1,7 @@
+package cn.ruc.readio.ui.userpage.ui.login;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginRegisterActivityViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

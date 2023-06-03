f9ca0cd2a91e7f8ac1a8eac92bcc26d35cca1a92
19800386590 <2363317668@qq.com>
2023-06-03 17:22:57 +0800

    阅读页更新Pageview

--- app/src/main/java/cn/ruc/readio/bookReadActivity/readBookActivity.java
+++ app/src/main/java/cn/ruc/readio/bookReadActivity/readBookActivity.java
@@ -1,6 +1,8 @@
 package cn.ruc.readio.bookReadActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerTabStrip;
+import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -8,12 +10,20 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import cn.ruc.readio.R;
 
 public class readBookActivity extends Activity {
+    ViewPager pager = null;
+    PagerTabStrip tabStrip = null;
+    ArrayList<View> viewContainer = new ArrayList<View>();
+    private int nPosition;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -33,10 +43,54 @@ public class readBookActivity extends Activity {
         String BookName = intent.getStringExtra("BookName");
         String Author = intent.getStringExtra("Author");
 
-        /*设置内容*/
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
-        TextView textView1 = findViewById(R.id.book_content);
-        textView1.setText(BookName+Author);
+        pager = (ViewPager) this.findViewById(R.id.viewpager);
+        View view1 = LayoutInflater.from(this).inflate(R.layout.item_bookview,null);
+        View view2 = LayoutInflater.from(this).inflate(R.layout.item_bookview,null);
+        View view3 = LayoutInflater.from(this).inflate(R.layout.item_bookview,null);
+        viewContainer.add(view1);
+        viewContainer.add(view2);
+        viewContainer.add(view3);
+        bookReadAdapter myAdapter = new bookReadAdapter();
+        pager.setAdapter(myAdapter);
+        pager.setCurrentItem(1,false);
+        myAdapter.setNewViewList(viewContainer);
 
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                nPosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state != pager.SCROLL_STATE_IDLE) return;
+                if (nPosition == viewContainer.size() - 1) {
+                    Log.d("mybug", toString().valueOf(nPosition));
+                    addPage("this is new page");
+                    pager.setCurrentItem(nPosition - 1, false);
+                    delFirstPage();
+                }
+            }
+
+            public void addPage(String text) {
+                LayoutInflater inflater = LayoutInflater.from(readBookActivity.this);
+                View view = inflater.inflate(R.layout.item_bookview, null);
+                TextView content = (TextView) view.findViewById(R.id.content);
+                content.setText(text);
+                viewContainer.add(view);
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+            public void delFirstPage() {
+                viewContainer.remove(0);
+                myAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }

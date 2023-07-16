613cf9030662bb6752bb058f740be540ac30da8e
19800386590 <2363317668@qq.com>
2023-07-16 23:20:43 +0800

    缺德专区开辟中

--- app/src/main/java/cn/ruc/readio/userPageActivity/FaithActivity.java
+++ app/src/main/java/cn/ruc/readio/userPageActivity/FaithActivity.java
@@ -5,7 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import cn.ruc.readio.R;
 
@@ -20,5 +27,22 @@ public class FaithActivity extends AppCompatActivity {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+        ImageView muyu = findViewById(R.id.muyuButton);
+//        TextView gongde = findViewById(R.id.gongde);
+        muyu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation_small = AnimationUtils.loadAnimation(FaithActivity.this,R.xml.knock_small);
+                Animation animation_big = AnimationUtils.loadAnimation(FaithActivity.this,R.xml.knock_big);
+                muyu.startAnimation(animation_small);
+                muyu.startAnimation(animation_big);
+//                gongde.setVisibility(View.VISIBLE);
+//                gongde.startAnimation(animation_big);
+//                gongde.setVisibility(View.INVISIBLE);
+
+
+            }
+        });
+
     }
 }
